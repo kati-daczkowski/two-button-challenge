b9957df52966ad322fbfe79cf4af942a22a23872
Kati Daczkowski <katidacz@gmail.com>
2020-09-20 12:27:38 -0400

    Initial Commit

---
+++ two button challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  two button challenge
+//
+//  Created by Kati Daczkowski on 9/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
